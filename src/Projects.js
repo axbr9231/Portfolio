9c1e4e842dc186a0aff421c287297a893853fa65
Axel Brostedt <axelbrostedt@gmail.com>
2019-12-26 10:19:04 +0100

    Adds styling to card. Card now spins on hover and has a back and front side

--- src/Projects.js
+++ src/Projects.js
@@ -37,14 +37,28 @@ const Projects = () => {
                     </div>
                     <div>
                         <div class="card" style={cardStyle.card}>
-                            <img src={require('./SwaggyThreadz.png')} alt="app_picture" style={imgStyle}></img>
-                            <div class="card-body" style={cardStyle.body}>
-                                <h5>Swaggy Threadz</h5>
-                                <p class="card-text">
-                                    A front-end project built for a clothing online store. Built in React, Redux and Bootstrap.
-                                </p>
-                                <p class="card-text"><small>Demo: <a href="http://axels-cocktail-maker.surge.sh/?fbclid=IwAR1uHTIeDGn7qy_RaUqKGeG5A-KxAJDK3FyTfC3PU9lWKiaQ3idvMiPH0pk">
-                                    SwaggyThreadz</a> <img src={require('./GitHub-Mark-32px.png')} alt="github_icon" className="githubLogo"></img> <a href="https://github.com/GHRBLD05/SwaggyThreadz"> Code</a></small></p>
+                            <div class="card_inner">
+                                <div class="card_front">
+                                    <img src={require('./SwaggyThreadz.png')} alt="app_picture" style={imgStyle}></img>
+                                    <div class="card-body" style={cardStyle.body}>
+                                        <h5>Swaggy Threadz</h5>
+                                        <p class="card-text">
+                                            A full stack project built for a clothing online store. Front end hosted through Express and back end through AWS.
+                                        </p>
+                                    </div> 
+                                </div>
+                                <div class="card_back">
+                                    <div class="card-body">
+                                    <p>React</p>
+                                    <p>Redux</p>
+                                    <p>Express</p>
+                                    <p>Node</p>
+                                    <p>PostgreSQL</p>
+                                    <p>BootStrap</p>
+                                    </div>
+                                    <p class="card-text"><small>Demo: <a href="http://axels-cocktail-maker.surge.sh/?fbclid=IwAR1uHTIeDGn7qy_RaUqKGeG5A-KxAJDK3FyTfC3PU9lWKiaQ3idvMiPH0pk">
+                                        SwaggyThreadz</a> <img src={require('./GitHub-Mark-32px.png')} alt="github_icon" className="githubLogo"></img> <a href="https://github.com/GHRBLD05/SwaggyThreadz"> Code</a></small></p>
+                                </div>  
                             </div>
                         </div>
                     </div>
@@ -65,9 +79,10 @@ const cardStyle = {
     card: {
         maxHeight: '350px',
         maxWidth: '250px',
-        backgroundColor: 'DarkSlateGray',
+        backgroundColor: 'transparent',
         border: 'solid black 2px',
-        minHeight: '344.84px'
+        minHeight: '344.84px',
+        minWidth: '250px'
     },
     body: {
         color: 'white',
